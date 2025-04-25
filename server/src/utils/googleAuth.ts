b48e1cdf8ce30aa4401e8e4f56ceb08c5b7e0c2b
Ayush Kumar Gupta <ayushkg97@gmail.com>
2025-04-25 15:29:36 -0700

    Checked in for first screen

---
+++ server/src/utils/googleAuth.ts
@@ -0,0 +1,20 @@
+import { google } from 'googleapis';
+
+export const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI
+);
+
+export const getCalendarClient = (tokens:any) => {
+    const auth = new google.auth.OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      process.env.GOOGLE_REDIRECT_URI
+    );
+  
+    auth.setCredentials(tokens);
+    return google.calendar({ version: 'v3', auth });
+  };
+
+
