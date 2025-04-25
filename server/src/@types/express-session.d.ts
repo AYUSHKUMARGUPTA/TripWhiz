a7022048e15b23487fee2ca1599be6d768361656
Ayush Kumar Gupta <ayushkg97@gmail.com>
2025-04-25 15:34:43 -0700

    Checked in for updated code

--- server/src/@types/express-session.d.ts
+++
@@ -1,8 +0,0 @@
-// src/types/express-session.d.ts
-import session from 'express-session';
-
-declare module 'express-session' {
-  interface SessionData {
-    tokens?: any; // You can replace `any` with the actual token structure
-  }
-}
